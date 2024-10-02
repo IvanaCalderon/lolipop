cf9e3802e4f312bbb28b07f52a2c4658c187ee71
ivanacalderon <ivanacalderonp@gmail.com>
2024-10-02 10:40:06 -0400

    Added Employees to admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
+
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
     panel "Pending Applications" do
       applications = Application.pending # Obtener solo las aplicaciones en estado "pending"
-      
+
       if applications.any?
         table_for applications do
           column("Candidate") { |application| application.candidate.name }
@@ -14,11 +15,30 @@ ActiveAdmin.register_page "Dashboard" do
           column("Actions") do |application|
             link_to "View", admin_application_path(application)
           end
-
         end
       else
         div class: "blank_slate" do
           span "No pending applications."
+        end
+      end
+    end
+
+    panel "Employees" do
+      employees = Employee.active # Obtener solo los empleados activos
+
+      if employees.any?
+        table_for employees do
+          column("Name") { |employee| employee.name }
+          column("Position") { |employee| employee.position.name }
+          column("Hire Date") { |employee| employee.hire_date.strftime("%B %d, %Y") }
+          column("Monthly Salary") { |employee| number_to_currency(employee.monthly_salary) }
+          column("Actions") do |employee|
+            link_to "View", admin_employee_path(employee)
+          end
+        end
+      else
+        div class: "blank_slate" do
+          span "No active employees."
         end
       end
     end
